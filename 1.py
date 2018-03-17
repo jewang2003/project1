d17ae92ee21b9fc25e6179cb530ba86e74c7fb6e
jewang2003 <36096384+jewang2003@users.noreply.github.com>
2018-03-17 15:42:13 +0900

    3/17 학습내용추가
    ㅎ

--- 1.py
+++ 1.py
@@ -106,3 +106,123 @@ print("나눗셈 결과:", input_a / input_b)
 
 # 숫자가 아닌 것을 숫자로 변형하려고 할 때
 int("안녕하세요")
+
+# format() 함수로 숫자를 문자열로 변환하기
+format_output = "{}".format(52273)
+
+# 출력하기
+print(type(format_output), format_output)
+
+# format() 함수로 숫자를 문자열로 변환하기
+format_output_a = "{}원".format(3000)
+format_output_b = "{} {} {}".format(1, 2, 3)
+format_output_c = "{} {} {}".format(1, "문자열", True)
+
+# 출력하기
+print(format_output_a)
+print(format_output_b)
+print(format_output_c)
+
+# "{} {}".format(1, 2, 3, 4, 5)
+# "{} {} {}".format(1, 2) #오류 발생 
+
+# 정수
+output_a = "{:d}".format(52)
+
+# 특정 칸에 출력하기
+output_b = "{:5d}".format(52) #5칸
+output_c = "{:10d}".format(52) #10칸
+
+# 빈 칸을 0으로 채우기
+output_d = "{:05d}".format(52) #양수
+output_e = "{:05d}".format(-52) #음수
+print("# 기본")
+print(output_a)
+print("# 특정 칸에 출력하기")
+print(output_b)
+print(output_c)
+print("# 빈 칸을 0으로 채우기")
+print(output_d)
+print(output_e)
+
+# 기호와 함께 출력하기
+output_f = "{:+d}".format(52) # 양수
+output_g = "{:+d}".format(-52) # 음수
+output_h = "{: d}".format(52) # 양수: 기호 부분 공백
+output_i= "{: d}".format(-52) # 음수: 기호 부분 공백
+
+print("# 기호와 함께 출력하기")
+print(output_f)
+print(output_g)
+print(output_h)
+print(output_i)
+
+# 조합하기
+output_h = "{:+5d}".format(52) # 기호를 뒤로 밀기: 양수
+output_i = "{:+5d}".format(-52) # 기호를 뒤로 밀기: 음수
+output_j = "{:=+5d}".format(52) # 기호를 앞으로 밀기: 양수
+output_k = "{:=+5d}".format(-52) # 기호를 앞으로 밀기: 음수
+output_l = "{:+05d}".format(52) # 0으로 채우기: 양수
+output_m = "{:+05d}".format(-52) # 0으로 채우기: 음수
+
+print("# 조합하기")
+print(output_h)
+print(output_i)
+print(output_j)
+print(output_k)
+print(output_l)
+print(output_m)
+
+output_a = "{:f}".format(52.273)
+output_b = "{:15f}".format(52.273) # 15칸 만들기
+output_c = "{:+15f}".format(52.273) # 15칸에 부호 추가하기
+output_d = "{:+015f}".format(52.273) # 15칸에 부호 추가하고 0으로 채우기
+
+print(output_a)
+print(output_b)
+print(output_c)
+print(output_d)
+
+output_a = "{:15.3f}".format(52.273)
+output_b = "{:15.2f}".format(52.273)
+output_c = "{:15.1f}".format(52.273)
+
+print(output_a)
+print(output_b)
+print(output_c)
+
+# 변수를 선언합니다
+str_a = "Hello Python Programming...!"
+# str_upper = str_a.upper()
+# str_lower = str_a.lower()
+
+# upper() 함수와 lower() 함수를 사용합니다.
+# print("str.upper():", str_upper)
+# print("str.lower():", str_lower)
+
+str_a.upper()
+print(str_a)
+str_a.lower()
+print(str_a)
+
+input_a = """
+    안녕하세요
+문자열의 함수를 알아봅니다
+"""
+
+
+# 문자열의 함수를 사용해봅니다.
+print("# 공백 제거 이전")
+print(input_a)
+print()
+
+print("# 공백 제거 이후")
+print(input_a.strip())
+
+# find() 함수와 rfind() 함수 사용하기
+output_a = "안녕안녕하세요".find("안녕")
+output_b = "안녕안녕하세요".rfind("안녕")
+
+# 출력하기
+print("find():", output_a)
+print("rfind():", output_b)
